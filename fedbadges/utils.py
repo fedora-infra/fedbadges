4dd16600b6a7d443b9d8ff84e4995b326555114a
Ralph Bean <rbean@redhat.com>
2013-06-18 10:42:15 -0400

    Lowercase subsitutions.  Workaround for wiki username.

--- fedbadges/utils.py
+++ fedbadges/utils.py
@@ -19,6 +19,8 @@ def construct_substitutions(msg):
                 ('.'.join([key1, key2]), val2)
                 for key2, val2 in construct_substitutions(msg[key1]).items()
             ]))
+        elif isinstance(msg[key1], basestring):
+            subs[key1] = msg[key1].lower()
         else:
             subs[key1] = msg[key1]
     return subs
