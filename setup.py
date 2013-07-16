d014c86ad71e9361174626fa043d4fcf2a494c2d
Ralph Bean <rbean@redhat.com>
2013-07-16 11:27:11 -0400

    0.2.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = file("README.rst").read().strip()
 
 setup(
     name='fedbadges',
-    version='0.2.1',
+    version='0.2.2',
     description='fedmsg consumer for awarding open badges',
     long_description=long_description,
     license='GPLv2+',
